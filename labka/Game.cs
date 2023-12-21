53690d39c1c06354a464f14f096557d39456a09c
min1k <masyuk0tanika@gmail.com>
2023-12-22 01:49:48 +0200

    Lab 3

---
+++ labka/Game.cs
@@ -0,0 +1,63 @@
+﻿
+using System.Security.AccessControl;
+//гра де на рейтинг грає лише один гравець. !!!!
+namespace labka
+{
+
+    public abstract class Game
+    {
+       public string OpponentName { get; private set; }
+        public int Rating { get; private set; }
+        public int Index { get; private set; }
+        public string Result { get; private set; }
+        public string GameType { get; private set; }
+
+        //private static int currentIndex = new Random().Next(1, 30000);
+        private static int currentIndex = 100;
+        private int GetNextIndex()
+        {
+            currentIndex++;
+            return currentIndex;
+        }
+
+
+        public Game(string opponentName, int rating, string result, string gameType)
+        {
+            OpponentName = opponentName;
+            Result = result;
+
+            Rating = rating;
+            Index = GetNextIndex();
+
+            GameType = gameType;
+        }
+
+        public abstract int CalculateRating();
+    }
+
+
+
+
+    public class StandartGame : Game
+    {
+        public StandartGame(string opponentName, int raiting, string result, string gameType) : base(opponentName, 10, result, gameType) // Рейтинг для стандартної гри
+        {
+        }
+        public override int CalculateRating()
+        {
+            return Rating;
+        }
+    }
+
+
+    public class WithoutRatingGame : Game
+    {
+        public WithoutRatingGame(string opponentName, int raiting, string result, string gameType) : base(opponentName, 0, result, gameType) { }
+
+        public override int CalculateRating()
+        {
+            return Rating;
+        }
+    }
+    //гра де на рейтинг грає лише один гравець.
+}
---
+++ labka/Program.cs
@@ -0,0 +1,41 @@
+﻿//по папках розділено!
+//проблемит з індексом.
+
+
+namespace labka
+{
+    //тип гри 
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            BonusAccount user1 = new BonusAccount("nub", 1, 2);
+            StandartAccount user2 = new StandartAccount("Pro", 5, 26);
+
+            HalfPointsForLossAccount user4 = new HalfPointsForLossAccount("bybka", 1, 2);
+
+
+            user1.WinGame("StandartGame", user2,10);
+            user1.WinGame("StandartGame", user2, 10);
+            user1.WinGame("StandartGame", user2,10);
+
+            user4.WinGame("StandartGame", user2, new Random().Next(1, 15));
+
+            user1.LoseGame("SinglPlayerGame", user2, new Random().Next(1, 15));
+            // user2.LoseGame(user1, new Random().Next(1, 6));
+            //user2.WinGame("StandartGame", user4, new Random().Next(1, 15));
+            //user2.WinGame("StandartGame", user1, new Random().Next(1, 15));
+            user1.GetStatus();
+            user2.GetStatus();
+        }
+    }
+}
+
+
+
+
+
